bff4a9b2b048296891f05ba2a1660a56fb2644d6
elguesabal <joseelguesabal@gmail.com>
2025-11-26 19:59:23 -0300

    corrigindo bugs com carregamento e upload de imagens em photo.js (falta revisar e limpar comentarios

--- src/functions/profile/photo.js
+++ src/functions/profile/photo.js
@@ -1,6 +1,6 @@
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system/legacy";
-import axios from "axios";
+// import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -8,7 +8,7 @@ import { logout } from "../logout.js";
 
 import { compatibleProfilePictures } from "../../compatibleImages.js";
 
-import API_URL from "../../Api.js";
+// import API_URL from "../../Api.js";
 import api from "../../config_axios.js";
 
 /**
@@ -39,11 +39,7 @@ async function selectPhoto(openModal) {
 	try {
 		const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-		if (status !== "granted") {
-			const err = new Error("Permissão a galeria negada!");
-			err.icon = "block";
-			throw (err);
-		}
+		if (status !== "granted") throw ({ icon: "block", text: "Permissão a galeria negada!" });
 		const photo = await ImagePicker.launchImageLibraryAsync({
 			mediaTypes: ["images"],
 			allowsEditing: true,
@@ -51,14 +47,11 @@ async function selectPhoto(openModal) {
 		});
 		if (photo.canceled) return (photo);
 		const type = photo.assets[0].uri.split(".").pop().toLowerCase();
-		if (!compatibleProfilePictures.includes(type)) {
-			const err = new Error(`Imagem ${type} não suportada!`);
-			err.icon = "image-not-supported";
-			throw (err);
-		}
+		if (!compatibleProfilePictures.includes(type)) throw ({ icon: "image-not-supported", text: `Imagem ${type} não suportada!` });
+		if (((await FileSystem.getInfoAsync(photo.assets[0].uri)).size / (1024 * 1024)) > 4) throw ({ icon: "image-not-supported", text: "Imagem muito grande!\nMáximo permitido: 4MB." });
 		return (photo);
 	} catch (error) {
-		openModal({ icon: error.icon, text: error.message, button: "Ok" });
+		openModal({ icon: error.icon, text: error.text, button: "Ok" });
 		throw (error);
 	}
 }
@@ -69,8 +62,11 @@ async function selectPhoto(openModal) {
  * @param setImg FUNCAO QUE DEFINE O LINK DA FOTO DE PERFIL DO USUARIO
  * @param setIsLogin FUNCAO DE CONTROLE DE LOGIN
  * @param openModal FUNCAO QUE ABRE O MODAL
+ * @param closeModal FUNCAO QUE FECHA O MODAL
+ * @param setData FUNCAO QUE SALVA O LINK DA FOTO DE PERFIL DO USUARIO
+ * @param data DADOS DO USUARIO
 */
-async function uploadPhoto(photo, setIsLogin, openModal) {
+async function uploadPhoto(photo, setIsLogin, openModal, closeModal, setData, data) {
 	// try {
 	// 	const formData = new FormData();
 	// 	formData.append("fotoPerfil", {
@@ -102,8 +98,9 @@ async function uploadPhoto(photo, setIsLogin, openModal) {
 
 
 
-	const formData = new FormData();		// COMECEI A REFATORAR
-	formData.append("fotoPerfil", {
+	openModal({ load: true });
+	const formData = new FormData();
+	formData.append("fotoPerfil", {												// ACHO Q AQUI TA TERMINADO E SO FALTA REVISAR
 		uri: photo.assets[0].uri,
 		name: `user.${photo.assets[0].uri.split(".").pop().toLowerCase()}`,
 		type: `image/${photo.assets[0].uri.split(".").pop().toLowerCase()}`
@@ -117,15 +114,16 @@ async function uploadPhoto(photo, setIsLogin, openModal) {
 		},
 		data: formData
 	});
-console.log(res.status)
-console.log(res.data)
-	// if (res.status === 204) return ;
-	if (res.status !== 0 && res.data !== "Network Error") {
-		console.log("veio akiiiiiiiii") // A IMAGEM ESTA RERENDERIZANDO O ELEMENTO E IMPEDINDO DE DEIXAR O AVISO APARECER??
-		// return (openModal({ icon: "wifi-off", text: "Sem conexão com o servidor.\nTentar novamente?", yes: (closeModal) => { closeModal(); uploadPhoto(photo, setIsLogin, openModal); }, no: (closeModal) => closeModal(), exit: (closeModal) => closeModal() }));
-		openModal({ icon: "wifi-off", text: "Sem conexão com o servidor.\nTentar novamente?", yes: (closeModal) => { closeModal(); uploadPhoto(photo, setIsLogin, openModal); }, no: (closeModal) => closeModal(), exit: (closeModal) => closeModal() })
+
+	if (res.status === 204) {
+		compatibleProfilePictures.map(async (format) => await FileSystem.deleteAsync(`${FileSystem.documentDirectory}user.${format}`, { idempotent: true }));
+		await FileSystem.copyAsync({ from: photo.assets[0].uri, to: `${FileSystem.documentDirectory}user.${photo.assets[0].uri.split(".").pop().toLowerCase()}` });
+		setData({ ...data, photo: photo.assets[0].uri });
+		closeModal();
+		return ;
 	}
-	if (res.status === 401) return (logout(setIsLogin));
+	if (res.status === 0 && res.data === "Network Error") return (openModal({ icon: "wifi-off", text: "Sem conexão com o servidor.\nTentar novamente?", yes: (closeModal) => { closeModal(); uploadPhoto(photo, setIsLogin, openModal); }, no: (closeModal) => closeModal(), exit: (closeModal) => closeModal() }));
+	if (res.status === 401) return (closeModal(), logout(setIsLogin));
 	if (res.status !== 204) return (openModal({ icon: "error-outline", text: `${res.status}\n${res.data}` }));
 }
 
@@ -143,12 +141,12 @@ export async function getPhoto(openModal, closeModal, data, setData, setIsLogin)
 		const photo = await selectPhoto(openModal);
 
 		if (photo.canceled) return (closeModal());
-		openModal({ load: true });
-		await uploadPhoto(photo, setIsLogin, openModal);
-		compatibleProfilePictures.map(async (format) => await FileSystem.deleteAsync(`${FileSystem.documentDirectory}user.${format}`, { idempotent: true }));
-		await FileSystem.copyAsync({ from: photo.assets[0].uri, to: `${FileSystem.documentDirectory}user.${photo.assets[0].uri.split(".").pop().toLowerCase()}` });
-		setData({ ...data, photo: photo.assets[0].uri });
-		closeModal();
+		// openModal({ load: true });
+		await uploadPhoto(photo, setIsLogin, openModal, closeModal, setData, data);
+		// compatibleProfilePictures.map(async (format) => await FileSystem.deleteAsync(`${FileSystem.documentDirectory}user.${format}`, { idempotent: true }));
+		// await FileSystem.copyAsync({ from: photo.assets[0].uri, to: `${FileSystem.documentDirectory}user.${photo.assets[0].uri.split(".").pop().toLowerCase()}` });
+		// setData({ ...data, photo: photo.assets[0].uri });
+		// closeModal(); // UE PQ ISSO TA AKI????
 	} catch (error) {
 
 	}
